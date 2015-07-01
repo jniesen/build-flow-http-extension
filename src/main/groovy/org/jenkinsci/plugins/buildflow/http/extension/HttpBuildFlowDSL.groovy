c418b34b6005d4b0d554e6a83d83e1cea539406e
Jonathan Niesen <jon.niesen@gmail.com>
2015-06-30 22:28:31 -0400

    Remove static modifier to prevent compilation errors.

--- src/main/groovy/org/jenkinsci/plugins/buildflow/http/extension/HttpBuildFlowDSL.groovy
+++ src/main/groovy/org/jenkinsci/plugins/buildflow/http/extension/HttpBuildFlowDSL.groovy
@@ -24,22 +24,22 @@ class HttpBuildFlowDSL {
   HTTPBuilder http
 
   class Method {
-    final static GET = groovyx.net.http.Method.GET
-    final static PUT = groovyx.net.http.Method.PUT
-    final static POST = groovyx.net.http.Method.POST
-    final static DELETE = groovyx.net.http.Method.DELETE
-    final static HEAD = groovyx.net.http.Method.HEAD
-    final static PATCH = groovyx.net.http.Method.PATCH
+    final GET = groovyx.net.http.Method.GET
+    final PUT = groovyx.net.http.Method.PUT
+    final POST = groovyx.net.http.Method.POST
+    final DELETE = groovyx.net.http.Method.DELETE
+    final HEAD = groovyx.net.http.Method.HEAD
+    final PATCH = groovyx.net.http.Method.PATCH
   }
 
   class ContentType {
-    final static ANY = groovyx.net.http.ContentType.ANY
-    final static TEXT = groovyx.net.http.ContentType.TEXT
-    final static JSON = groovyx.net.http.ContentType.JSON
-    final static XML = groovyx.net.http.ContentType.XML
-    final static HTML = groovyx.net.http.ContentType.HTML
-    final static URLENC = groovyx.net.http.ContentType.URLENC
-    final static BINARY = groovyx.net.http.ContentType.BINARY
+    final ANY = groovyx.net.http.ContentType.ANY
+    final TEXT = groovyx.net.http.ContentType.TEXT
+    final JSON = groovyx.net.http.ContentType.JSON
+    final XML = groovyx.net.http.ContentType.XML
+    final HTML = groovyx.net.http.ContentType.HTML
+    final URLENC = groovyx.net.http.ContentType.URLENC
+    final BINARY = groovyx.net.http.ContentType.BINARY
   }
 
   def HttpBuildFlowDSL(FlowDelegate flowDelegate, HTTPBuilder httpBuilder) {
